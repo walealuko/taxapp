b32a25698744723b384afce7432d337520ffdf76
walealuko <bigwalealuko@yahoo.com>
2026-05-15 14:45:58 -0500

    chore: remove tracked dist directory and update babel config

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,6 @@ module.exports = function(api) {
 
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module-resolver', { root: ['./'], alias: { '@': './' } }],
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
